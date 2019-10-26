36ec73ce50c03550a15653254d391be1f0318780
Rodolfo Macbook <rodolfo.mansur@linuxplace.com.br>
2019-10-26 18:38:44 -0300

    VPC OK

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -33,12 +33,44 @@ variable "region" {
   default = "us-east-1"
 }
 
-provider "aws" {
-  profile = "linuxplaceqas"
-  region  = var.region     
+variable "azs" {
+  default = [
+    "us-east-1a",
+    "us-east-1b",
+    "us-east-1c"
+  ]
+}
+
+variable "cidrsRegion" {
+  default = [
+    "10.21.0.0/16"
+  ]
+}
+
+variable "cidrsSubnet" {
+  default = [
+    "10.21.8.0/21",
+    "10.21.16.0/21",
+    "10.21.24.0/21",
+    "10.21.32.0/21",
+    "10.21.40.0/21",
+    "10.21.48.0/21",
+    "10.21.56.0/21",
+    "10.21.64.0/21",
+    "10.21.72.0/21",
+    "10.21.80.0/21",
+  ]
 }
 
 
+
+
+
+
+provider "aws" {
+  profile = "linuxplaceqas"
+  region  = var.region
+}
 
 
 module "vpc" {
@@ -46,32 +78,24 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "K8S-Network"
-  cidr = "10.21.0.0/16"
+  cidr = var.cidrsRegion[0] #"10.21.0.0/16"
 
   # Enable DNS
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   # Enable NAT Gateway to Private Subnet using single NAT
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  enable_nat_gateway = true
+  single_nat_gateway = true
 
 
-  azs             = ["eu-east-1a", "eu-east-1b", "eu-east-1c"]
-  private_subnets = [ "10.21.0.0/16", "10.21.8.0/21", "10.21.16.0/21" ]
+  azs             = var.azs                                    #["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = [var.cidrsSubnet[0], var.cidrsSubnet[1], var.cidrsSubnet[2]] #["10.21.0.0/16", "10.21.8.0/21", "10.21.16.0/21"]
+  public_subnets  = [var.cidrsSubnet[3], var.cidrsSubnet[4], var.cidrsSubnet[5]] #["10.21.24.0/21", "10.21.32.0/21", "10.21.40.0/21"]
 
-  private_subnet_tags = {
-    Name = "K8S-Private"
-  } 
-
-  public_subnets  = [ "10.21.24.0/21", "10.21.32.0/21", "10.21.40.0/21" ]
-  
-  public_subnet_tags  = {
-    Name = "K8S-Public"
-  }
 
   # suffix to append to private subnets name
-  private_subnet_suffix = "k8s"
+  #private_subnet_suffix = "k8s"
 
   tags = {
     Owner       = "Rodolfo"
